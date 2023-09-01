1278843144f2a0bc2b8c214b0faeed954f5fa0c5
Jaesung Park <richdad6208@gmail.com>
2023-09-02 08:32:54 +0900

    question word reset ing...

--- 행맨게임/main.js
+++ 행맨게임/main.js
@@ -6,13 +6,14 @@ const startButton = document.querySelector(".choice__start__button");
 const timeLeft = document.querySelector(".choice__time");
 const pushAlphabetWrapper = document.querySelector(".choice__alphabet-wrapper");
 const answer = document.querySelector(".choice__middle__body");
+const leftChance = document.querySelector(".choice__chance");
+const gameOver = document.querySelector(".gameover-screen");
 
-function changeResultImage(num) {
-  resultImage.setAttribute("src", `./images/hang-${num}.png`);
-}
 const firstWord = {
   questionWord: "",
   changedWord: "",
+  leftCount: 7,
+  leftTime: 60,
   choiceWord() {
     const randomNumber = Math.ceil(Math.random() * 10);
     const randomWord = words[randomNumber].word;
@@ -22,6 +23,7 @@ const firstWord = {
 };
 
 function printWord(alphabet) {
+  console.log(beforeWord, afterWord);
   const beforeWord = firstWord.questionWord;
   const afterWord = firstWord.changedWord;
   const result = checkWord(beforeWord, afterWord, alphabet);
@@ -39,32 +41,64 @@ function checkWord(beforeWord, afterWord, alphabet) {
     }
   }
   arr.forEach((item) => afterArr.splice(item, 1, alphabet));
-  console.log(arr);
   return afterArr.join("");
 }
 async function decreaseTime() {
-  let TOTAL_TIME = 60;
+  firstWord.leftTime = 60;
   startButton.style.opacity = "0";
-  while (TOTAL_TIME > 0) {
+  while (firstWord.leftTime > 0) {
     await sleep(1000);
-    TOTAL_TIME--;
-    timeLeft.innerText = `time: ${TOTAL_TIME}`;
+    firstWord.leftTime--;
+    timeLeft.innerText = `time: ${firstWord.leftTime}`;
   }
+  checkChance();
   startButton.style.opacity = "1";
 }
 startButton.addEventListener("click", start);
 
-function start() {
-  decreaseTime();
-  firstWord.choiceWord();
+function reactiveAlphabetButton() {
+  [...pushAlphabetWrapper.children].forEach((item) => {
+    item.removeAttribute("disabled");
+    item.style.opacity = "1";
+  });
 }
 
-pushAlphabetWrapper.addEventListener("click", pushAlphabet);
-
+function start(e) {
+  matchChangeToResult(7);
+  reactiveAlphabetButton();
+  firstWord.choiceWord();
+  decreaseTime();
+  pushAlphabetWrapper.addEventListener("click", pushAlphabet);
+}
+function retry(e) {
+  if (e.target.className === "gameover__retry") {
+    gameOver.style.left = "2000px";
+    gameOver.style.opacity = "0";
+    start();
+  }
+}
+gameOver.addEventListener("click", retry);
+function checkChance() {
+  if (firstWord.leftCount === 0 || firstWord.leftTime === 0) {
+    resultImage.setAttribute("src", `./images/hang-0.png`);
+    gameOver.style.left = "0";
+    gameOver.style.opacity = "1";
+  }
+}
+function matchChangeToResult(num) {
+  resultImage.setAttribute("src", `./images/hang-${num}.png`);
+}
 function pushAlphabet(e) {
   if (e.target.className === "alphabet") {
-    const word = e.target.innerText.toLowerCase();
-    e.target.style.opacity = "0";
-    printWord(word);
+    if (firstWord.leftCount > 0) {
+      firstWord.leftCount--;
+      leftChance.innerHTML = `chances: ${firstWord.leftCount}`;
+      matchChangeToResult(firstWord.leftCount);
+      checkChance();
+      const word = e.target.innerText.toLowerCase();
+      e.target.style.opacity = "0";
+      e.target.setAttribute("disabled", "");
+      printWord(word);
+    }
   }
 }
